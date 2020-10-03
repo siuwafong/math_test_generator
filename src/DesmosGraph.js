809ff735719d2ebbb269db880a2eaa1fcddbe367
Wilson Fong <wilsonfong82@gmail.com>
2020-10-03 14:49:01 -0400

    add Desmos settings

--- src/DesmosGraph.js
+++ src/DesmosGraph.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import Desmos from 'desmos'
+import './DesmosGraph.css'
 
-
-const DesmosGraph = ({ graphfunction, graphid }) => {
+const DesmosGraph = ({ 
+    graphfunction = 'y=x', 
+    graphid = Math.ceil(Math.random() * 1000000),
+    keypad = false, 
+    expressions = false ,
+    lockViewport = false,
+    pointsOfInterest = true,
+    xAxisLabel = 'x',
+    yAxisLabel = 'y',
+    degreeMode = false,
+    xAxisStep = 0,
+    yAxisStep = 0,
+    }) => {
 
 
     let calcDiv = document.getElementById("calcDiv")
@@ -11,8 +23,20 @@ const DesmosGraph = ({ graphfunction, graphid }) => {
     elt.style.width = '500px'
     elt.style.height = '350px'
 
-    const calculator = Desmos.GraphingCalculator(elt)
-    calculator.setExpression({ id: 'graph1', latex: 'y=x^2' })
+    const calculator = Desmos.GraphingCalculator(elt, {
+        keypad: keypad,
+        expressions: expressions,
+        xAxisArrowMode: Desmos.AxisArrowModes.POSITIVE,
+        yAxisArrowMode: Desmos.AxisArrowModes.POSITIVE,
+        lockViewport: lockViewport,
+        pointsOfInterest: pointsOfInterest,
+        xAxisLabel: xAxisLabel,
+        yAxisLabel: yAxisLabel,
+        degreeMode: degreeMode,
+        xAxisStep: xAxisStep,
+        yAxisStep: yAxisStep
+    })
+    calculator.setExpression({ id: graphid, latex: graphfunction })
 
     // document.body.prepend(elt)
 
